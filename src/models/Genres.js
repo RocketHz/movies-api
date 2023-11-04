e3e5f92d8d55a51a22b3f7d3756f7ed5bbe5b3b2
Ronni Hernandez <ronnihn28@gmail.com>
2023-11-04 15:22:09 -0400

    add models controllers and routes

---
+++ src/models/Genres.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const Genres = sequelize.define('genres', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, { 
+    timestamps: false
+});
+
+module.exports = Genres;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const moviesRouter = require('./movies.routes');
+const genresRouter = require('./genres.routes');
+const actorsRouter = require('./actors.routes');
+const directorsRouter = require('./directors.routes');
 const router = express.Router();
 
 // colocar las rutas aquí
+router.use("/movies", moviesRouter);
+router.use("/genres", genresRouter);
+router.use("/actors", actorsRouter);
+router.use("/directors", directorsRouter);
 
 
 module.exports = router;
